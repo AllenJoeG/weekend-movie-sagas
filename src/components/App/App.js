7a1eb3e59f6b9aa0efae8846283c9a3d5238c97c
AllenJoeG <allen.joeg@gmail.com>
2021-12-11 17:11:46 -0600

    Custom theme found its home in index.js. ThemeProvider wrapping App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,32 +5,41 @@ import MovieList from '../MovieList/MovieList'
 import Details from '../Details/Details.jsx';
 import AddMovie from '../AddMovie/AddMovie';
 
+//MUI stuff
+import { Box } from '@mui/material';
+
 function App() {
   return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
+    <Box   
+      bgcolor="primary.main"
+      className="App"
+    >
       <Router>
-        <nav display="flex">
-          <Link to="/addmovie">Add A Movie</Link>
-          <span> 🍿 </span>
-          <Link to="/"> Movie Gallery</Link>
-          <span> 🍿 </span>
-          <Link to="/details"> Movie Details</Link>
-        </nav>
-                
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        {/* Details page */}
-        <Route exact path="/details">
-          <Details/>
-        </Route>
-        {/* Add Movie page */}
-        <Route exact path="/addmovie">
-          <AddMovie />
-        </Route>
+        <Box bgcolor="secondary.dark">
+          <h1>The Movies Saga!</h1>
+          <nav display="flex">
+            <Link to="/addmovie">Add A Movie</Link>
+            <span> 🍿 </span>
+            <Link to="/"> Movie Gallery</Link>
+            <span> 🍿 </span>
+            <Link to="/details"> Movie Details</Link>
+          </nav>
+        </Box>
+        <Box>
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
+          {/* Details page */}
+          <Route exact path="/details">
+            <Details/>
+          </Route>
+          {/* Add Movie page */}
+          <Route exact path="/addmovie">
+            <AddMovie />
+          </Route>
+        </Box>
       </Router>
-    </div>
+    </Box>
   );
 }
 
