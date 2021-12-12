ec582618a755ab475060c87ad95156a46817d1c7
AllenJoeG <allen.joeg@gmail.com>
2021-12-12 17:32:04 -0600

    Nav bar probably done. Grid applied for spacing on all mobile displays. Structural Griditem employed for bottom margin on header. hacky.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ import Details from '../Details/Details.jsx';
 import AddMovie from '../AddMovie/AddMovie';
 
 //MUI stuff
-import { Box, Button, AppBar, Typography } from '@mui/material';
+import { Box, Button, AppBar, Typography, Grid } from '@mui/material';
 import Link from '@mui/material/Link';
 import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
 
@@ -18,36 +18,50 @@ function App() {
     >
       <Router>
         <Box sx={{flexGrow: 1}}>
-          <AppBar position="static" spacing={2}>
-            <nav display="flex" justify="space-between">
-              <Typography float="left" variant="h3" color="secondary.dark" component="div" sx={{ flexGrow: 1 }}>
-                <LocalMoviesIcon/>Epic Sagas<LocalMoviesIcon/>
-              </Typography>
-              <Link 
-                component={RouterLink}
-                color="inherit"
-                underline="hover" 
-                to="/addmovie"
-                onClick={() => {console.info("Link to Add Movie");}}
+          <AppBar position="static">
+            <nav display="flex" >
+              <Grid container spacing={2} 
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="baseline"
               >
-                <Button variant="contained" color="secondary">Add Movie</Button>
-              </Link>
-              <Link 
-                component={RouterLink}
-                underline="hover"
-                to="/"
-                onClick={() => {console.info("Link to Movie Gallery");}}
-              > 
-                <Button variant="contained" color="secondary">Gallery</Button>
-              </Link>
-              <Link 
-                component={RouterLink} 
-                underline="hover" 
-                to="/details"
-                onClick={() => {console.info("Link to Single Movie Detail");}}
-              >
-                <Button variant="contained" color="secondary">Details</Button>
-              </Link>
+                <Grid item xs={12}>
+                  <Typography float="left" variant="h3" color="secondary.dark" component="div" sx={{ flexGrow: 1 }}>
+                    <LocalMoviesIcon/>Epic Sagas<LocalMoviesIcon/>
+                  </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Link 
+                    component={RouterLink}
+                    underline="hover" 
+                    to="/addmovie"
+                    onClick={() => {console.info("Link to Add Movie");}}
+                  >
+                    <Button fullWidth variant="contained" color="secondary">Add Movie</Button>
+                  </Link>
+                </Grid>
+                <Grid item xs={4}>
+                  <Link 
+                    component={RouterLink}
+                    underline="hover"
+                    to="/"
+                    onClick={() => {console.info("Link to Movie Gallery");}}
+                  > 
+                    <Button fullWidth variant="contained" color="secondary">Gallery</Button>
+                  </Link>
+                </Grid>
+                <Grid item xs={4}>
+                  <Link 
+                    component={RouterLink} 
+                    underline="hover" 
+                    to="/details"
+                    onClick={() => {console.info("Link to Single Movie Detail");}}
+                  >
+                    <Button fullWidth variant="contained" color="secondary">Details</Button>
+                  </Link>
+                </Grid>
+                <Grid item xs={12}></Grid>
+              </Grid>
             </nav>
           </AppBar>
         </Box>
